504ec79674fbfac218070f03f9ab58d1c2a11d79
Kendrick Williams <williams.kendrick@gmail.com>
2023-07-17 18:51:34 -0400

    Updated tab 1; remved dist files from source control

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,21 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonButton, IonContent, IonIcon, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonSegment, IonSegmentButton } from '@ionic/react';
+
+import { addOutline, chevronDownOutline } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader mode='md' className="ion-no-border">
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonButtons slot="start">
+          <IonButton>
+            <IonIcon slot="end" icon={chevronDownOutline}></IonIcon>
+            July 2023
+          </IonButton>
+        </IonButtons>
+        <IonButtons slot="end">
+          <IonButton>
+            <IonIcon slot="icon-only" icon={addOutline}></IonIcon>
+          </IonButton>
+        </IonButtons>
         </IonToolbar>
+        <IonSegment value="planned" mode='ios'>
+          <IonSegmentButton value="planned" mode='ios'>
+            <IonLabel>Planned</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="spent" mode='ios'>
+            <IonLabel>Spent</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="remaining" mode='ios'>
+            <IonLabel>Remaining</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
